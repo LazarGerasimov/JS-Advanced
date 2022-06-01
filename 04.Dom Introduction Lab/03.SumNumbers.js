f07f3176c413616f919a2520c514505c418be8ab
Laz <lazgerasimov@gmail.com>
2022-06-01 12:38:20 +0100

    adding 3

---
+++ 04.Dom Introduction Lab/03.SumNumbers.js
@@ -0,0 +1,9 @@
+function calc() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const result = Number(num1) + Number(num2);
+
+    document.getElementById("sum").value = result;
+
+    
+}
