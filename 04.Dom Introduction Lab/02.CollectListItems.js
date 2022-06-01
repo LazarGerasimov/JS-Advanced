b52ab6b5149699d3856192b3a305a87bc8e52d18
Laz <lazgerasimov@gmail.com>
2022-06-01 12:37:38 +0100

    adding 2

---
+++ 04.Dom Introduction Lab/02.CollectListItems.js
@@ -0,0 +1,7 @@
+function extractText() {
+    const items = Array.from(document.querySelectorAll('li'));
+    const result = items.map(e => e.textContent).join(`\n`);
+
+    document.getElementById('result').value = result;
+
+}
---
+++ 04.Dom Introduction Lab/app.js
@@ -0,0 +1,3 @@
+function print() {
+    console.log("code from inline script");
+    }
