2ab184e1c6b30f98cd07318919e42ca005a6ad7b
Laz <lazgerasimov@gmail.com>
2022-06-03 13:03:49 +0100

    adding 4

---
+++ 07.DOM Manipulation and Events Exercise/04. Fill-Dropdown/dropdown.js
@@ -0,0 +1,17 @@
+function addItem() {
+    
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+    let select = document.getElementById('menu');
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = newItemText.value;
+    optionElement.value = newItemValue.value;
+
+    select.appendChild(optionElement);
+
+    
+    newItemText.value = '';
+    newItemValue.value = '';
+
+}
