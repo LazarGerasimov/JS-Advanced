9d14e6156a9b04126a0a40c4bfb09d62d46105ca
Laz <lazgerasimov@gmail.com>
2022-06-06 13:10:08 +0100

    adding 5

---
+++ 08.Advanced Functions/05. Functional Sum.js
@@ -0,0 +1,16 @@
+function add(n) {
+
+    const inner = function (a) {
+        n += a;
+        return inner;
+    };
+
+    inner.toString = function () {
+        return n;
+    }
+
+    return inner;
+}
+
+console.log(add(1).toString());
+console.log(add(1)(6)(-3).toString());
