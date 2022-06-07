62672843baac0b3e0db16b0cd1d907c770c1335b
Laz <lazgerasimov@gmail.com>
2022-06-07 13:11:18 +0100

    adding 8

---
+++ 08.Advanced Functions/08. Next Article/script.js
@@ -0,0 +1,17 @@
+function getArticleGenerator(articles) {
+    
+    const div = document.getElementById('content');
+
+
+
+    return function () {
+        
+        const article = articles.shift();
+
+        if (article != undefined) {
+        const element = document.createElement('article');
+        element.textContent = article;
+        div.appendChild(element);
+    }
+    }
+}
